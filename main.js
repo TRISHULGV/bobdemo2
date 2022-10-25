1835d08f3446edcb65d35866595d0efcabfd7edd
TRISHULGV <93774023+TRISHULGV@users.noreply.github.com>
2022-10-25 17:28:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// Smooth scroll
+
+$('.navbar a').on('click', function(e) {
+    if(this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        },
+         800
+        );
+    }
+});
+
+
+$('.home-link').on('click', function(e) {
+    if(this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        },
+         1000
+        );
+    }
+});
